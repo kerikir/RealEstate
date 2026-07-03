fa0dc493709fd75e83a028c82511794a0c1e638d
kerikir <vlasov-kerik@mail.ru>
2026-07-03 15:14:09 +0300

    image added to Splash Screen

--- app/src/main/java/com/kerikir/realestate/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/splash/SplashScreen.kt
@@ -1,12 +1,18 @@
 package com.kerikir.realestate.feature.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.kerikir.realestate.R
 
 @Composable
 @Preview
@@ -16,6 +22,15 @@ fun SplashScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
             .verticalScroll(rememberScrollState())
-    ) { }
+    ) {
+        Image(
+            painter = painterResource(
+                R.drawable.splash),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
