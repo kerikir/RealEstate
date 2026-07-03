c645f90a0cfb1d3b0b6aaa7f05ab453f03f47d2a
kerikir <vlasov-kerik@mail.ru>
2026-07-03 16:25:22 +0300

    added a google account login button on splash screen

--- app/src/main/java/com/kerikir/realestate/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/splash/SplashScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -88,6 +91,14 @@ fun SplashScreen(
                     color = Color.White,
                 )
             }
+            Spacer(modifier = Modifier.width(16.dp))
+            Image(
+                painter = painterResource(R.drawable.google),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp),
+            )
         }
+
+        Spacer(Modifier.height(24.dp))
     }
 }
