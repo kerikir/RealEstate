e4636f3dd8de45dc0cecf99f01cbbbaa13d2d039
kerikir <vlasov-kerik@mail.ru>
2026-07-04 16:25:25 +0300

    the color of the text field in the composable function is set

--- app/src/main/java/com/kerikir/realestate/ui/components/SearchRow.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/SearchRow.kt
@@ -1,10 +1,15 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,7 +17,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kerikir.realestate.R
@@ -37,7 +45,27 @@ fun SearchRow() {
                     text = "Street, Address, City...",
                     color = colorResource(R.color.grey)
                 )
-            }
+            },
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.search_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(18.dp)
+                )
+            },
+            modifier = Modifier
+                .weight(1f)
+                .height(52.dp)
+                .clip(RoundedCornerShape(30.dp)),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = colorResource(R.color.white),
+                unfocusedContainerColor = colorResource(R.color.white),
+                disabledContainerColor = colorResource(R.color.white),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                cursorColor = colorResource(R.color.black),
+            ),
         )
     }
 }
