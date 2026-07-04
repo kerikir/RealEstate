5cfdafe064fef93b9e0fa5dc676f90b2c8b7b4c6
kerikir <vlasov-kerik@mail.ru>
2026-07-04 13:51:40 +0300

    added a data model for the property

--- app/src/main/java/com/kerikir/realestate/core/model/Property.kt
+++ app/src/main/java/com/kerikir/realestate/core/model/Property.kt
@@ -1,5 +1,15 @@
 package com.kerikir.realestate.core.model
 
 data class Property(
-    
+    val type: String,
+    val title: String,
+    val address: String,
+    val picPath: String,
+    val price: Int,
+    val bed: Int,
+    val bath: Int,
+    val size: Int,
+    val isGarage: Boolean,
+    val score: Double,
+    val description: String,
 )
