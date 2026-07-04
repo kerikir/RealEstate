d578719f831cfde335a8b85f94cb964cab51993d
kerikir <vlasov-kerik@mail.ru>
2026-07-04 14:27:58 +0300

    added the top panel to the main screen

--- app/src/main/java/com/kerikir/realestate/feature/home/MainScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/home/MainScreen.kt
@@ -1,2 +1,32 @@
 package com.kerikir.realestate.feature.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kerikir.realestate.R
+import com.kerikir.realestate.ui.components.HeaderSection
+
+
+@Composable
+@Preview
+fun MainScreen() {
+    val items = remember { sampleProperties() }
+    
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .background(colorResource(R.color.light_grey)),
+        contentPadding = PaddingValues(bottom = 100.dp)
+    ) {
+        item { HeaderSection() }
+    }
+}
