54cae65212b7949e4462ae67790809851a620c30
kerikir <vlasov-kerik@mail.ru>
2026-07-04 13:56:49 +0300

    added sample data

--- app/src/main/java/com/kerikir/realestate/feature/home/SampleData.kt
+++ app/src/main/java/com/kerikir/realestate/feature/home/SampleData.kt
@@ -1,2 +1,58 @@
 package com.kerikir.realestate.feature.home
 
+import com.kerikir.realestate.core.model.Property
+
+fun sampleProperties(): List<Property> = listOf(
+    Property(
+        type = "Apartment",
+        title = "Royal Apartment",
+        address = "LosAngles LA",
+        picPath = "pic_1",
+        price = 1500,
+        bed = 2,
+        bath = 3,
+        size = 350,
+        isGarage = true,
+        score = 4.5,
+        description = "This 2 bed /3 bath home boasts an enormous, open-living plan..."
+    ),
+    Property(
+        type = "House",
+        title = "House with Great View",
+        address = "NewYork NY",
+        picPath = "pic_2",
+        price = 800,
+        bed = 1,
+        bath = 2,
+        size = 500,
+        isGarage = false,
+        score = 4.9,
+        description = "This 1 bed /2 bath home boasts an enormous, open-living plan..."
+    ),
+    Property(
+        type = "Villa",
+        title = "Royal Villa",
+        address = "LosAngles LA",
+        picPath = "pic_3",
+        price = 999,
+        bed = 2,
+        bath = 1,
+        size = 400,
+        isGarage = true,
+        score = 4.7,
+        description = "This 2 bed /1 bath home boasts an enormous, open-living plan..."
+    ),
+    Property(
+        type = "House",
+        title = "beauty house",
+        address = "NewYork NY",
+        picPath = "pic_4",
+        price = 1750,
+        bed = 3,
+        bath = 2,
+        size = 1100,
+        isGarage = true,
+        score = 4.3,
+        description = "This 3 bed /2 bath home boasts an enormous, open-living plan..."
+    )
+)
