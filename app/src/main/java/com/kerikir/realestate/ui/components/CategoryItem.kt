4f318c75450c417ef5e83bccbc774d135e68df86
kerikir <vlasov-kerik@mail.ru>
2026-07-05 12:13:18 +0300

    added an icon to the real estate category

--- app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
@@ -1,8 +1,11 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -10,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kerikir.realestate.R
@@ -32,8 +36,14 @@ fun CategoryItem(
                 .background(colorResource(R.color.white)),
             contentAlignment = Alignment.Center,
         ) {
-
+            Image(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+            )
         }
+
+        Spacer(Modifier.height(8.dp))
     }
 }
 
