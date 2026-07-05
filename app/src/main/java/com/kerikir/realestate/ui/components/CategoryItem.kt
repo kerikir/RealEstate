bfdc5781fc36bbb4d7479ef4cfa41fd236a2bbb7
kerikir <vlasov-kerik@mail.ru>
2026-07-05 12:17:53 +0300

    added a heading to the real estate category

--- app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
@@ -8,14 +8,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kerikir.realestate.R
 
 
@@ -44,6 +49,16 @@ fun CategoryItem(
         }
 
         Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = title,
+            color = colorResource(R.color.black),
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
     }
 }
 
