ecb0b7129ed7ed95c08388dbc03d7f4001d2e197
kerikir <vlasov-kerik@mail.ru>
2026-07-05 11:56:53 +0300

    added a composable function for categories

---
+++ app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
@@ -0,0 +1,13 @@
+package com.kerikir.realestate.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CategoryItem(
+    icon: Int,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+}
