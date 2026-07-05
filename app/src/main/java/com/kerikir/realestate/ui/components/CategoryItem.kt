3a3ce129a72db33cfccbdc6900194a808b504d20
kerikir <vlasov-kerik@mail.ru>
2026-07-05 12:03:36 +0300

    added a container for the category

--- app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
@@ -1,13 +1,37 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kerikir.realestate.R
 
 @Composable
 fun CategoryItem(
     icon: Int,
     title: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(45.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(colorResource(R.color.white)),
+            contentAlignment = Alignment.Center,
+        ) {
+
+        }
+    }
 }
