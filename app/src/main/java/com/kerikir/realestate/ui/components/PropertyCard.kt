08801e453e8244e3779c59306a8dd3d66d909ad5
kerikir <vlasov-kerik@mail.ru>
2026-07-05 14:36:35 +0300

    added an image of the property on the property card

--- app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
@@ -1,6 +1,8 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,11 +11,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kerikir.realestate.R
 import com.kerikir.realestate.core.model.Property
+import com.kerikir.realestate.util.gedDrawableId
 
 
 @Composable
@@ -32,7 +37,22 @@ fun PropertyCard(
             .fillMaxWidth()
             .clip(RoundedCornerShape(30.dp))
             .background(white),
-    ) { }
+    ) {
+
+        Box {
+            Image(
+                painter = painterResource(gedDrawableId(item.picPath)),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(white)
+                    .height(233.dp)
+                    .padding(12.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                contentScale = ContentScale.Crop,
+            )
+        }
+    }
 }
 
 
