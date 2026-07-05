3f941f0821a2415d31f69adb5ddbfd4f4ac0ed4b
kerikir <vlasov-kerik@mail.ru>
2026-07-05 15:16:01 +0300

    added an icon to the chip to describe the property

--- app/src/main/java/com/kerikir/realestate/ui/components/MetaChip.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/MetaChip.kt
@@ -1,6 +1,15 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -8,5 +17,15 @@ fun MetaChip(
     icon: Int,
     text: String,
 ) {
-    
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(icon),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+        )
+        
+        Spacer(Modifier.width(8.dp))
+    }
 }
