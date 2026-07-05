3d0f59c1a2b4b8be10864d7c5d64e1f868c7589f
kerikir <vlasov-kerik@mail.ru>
2026-07-05 13:22:30 +0300

    added a composable function for the real estate card

---
+++ app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
@@ -0,0 +1,15 @@
+package com.kerikir.realestate.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.kerikir.realestate.core.model.Property
+
+
+@Composable
+fun PropertyCard(
+    item: Property
+) {
+    Column(
+
+    ) { }
+}
