87eef19ba6b9c9704d3e95a0f3b4959417eb6bf9
kerikir <vlasov-kerik@mail.ru>
2026-07-05 15:17:52 +0300

    added text to chip to describe real estate

--- app/src/main/java/com/kerikir/realestate/ui/components/MetaChip.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/MetaChip.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kerikir.realestate.R
 
 
 @Composable
@@ -25,7 +29,13 @@ fun MetaChip(
             contentDescription = null,
             modifier = Modifier.size(16.dp),
         )
-        
+
         Spacer(Modifier.width(8.dp))
+
+        Text(
+            text = text,
+            color = colorResource(R.color.grey),
+            fontSize = 10.sp,
+        )
     }
 }
