4603f92533bf907e824838f7a0d0d7c9bb2c7b5c
kerikir <vlasov-kerik@mail.ru>
2026-07-05 12:07:26 +0300

    added the preview function to view the category

--- app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/CategoryItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kerikir.realestate.R
 
+
 @Composable
 fun CategoryItem(
     icon: Int,
@@ -34,4 +35,15 @@ fun CategoryItem(
 
         }
     }
+}
+
+
+
+@Composable
+@Preview
+fun CategoryItemPreview() {
+    CategoryItem(
+        icon = R.drawable.cat_1,
+        title = "Apartments"
+    )
 }
