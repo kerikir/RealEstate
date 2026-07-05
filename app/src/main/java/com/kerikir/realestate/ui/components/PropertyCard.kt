42b38d019446b41d76d7997d2e4945b275581b10
kerikir <vlasov-kerik@mail.ru>
2026-07-05 13:28:19 +0300

    added background for the property card

--- app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
@@ -1,8 +1,18 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kerikir.realestate.R
 import com.kerikir.realestate.core.model.Property
 
 
@@ -10,8 +20,18 @@ import com.kerikir.realestate.core.model.Property
 fun PropertyCard(
     item: Property
 ) {
-    Column(
+    val white = colorResource(R.color.white)
+    val blue = colorResource(R.color.blue)
+    val black = colorResource(R.color.black)
+    val grey = colorResource(R.color.grey)
 
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .height(320.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(30.dp))
+            .background(white),
     ) { }
 }
 
