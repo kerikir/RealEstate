039f4769651c85c1fd797ca875b3b5e9728ad4fd
kerikir <vlasov-kerik@mail.ru>
2026-07-05 13:24:56 +0300

    added a composable function to view the property card

--- app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/PropertyCard.kt
@@ -2,6 +2,7 @@ package com.kerikir.realestate.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.kerikir.realestate.core.model.Property
 
 
@@ -12,4 +13,25 @@ fun PropertyCard(
     Column(
 
     ) { }
+}
+
+
+
+@Composable
+@Preview
+fun PropertyCardPreview() {
+    val property = Property(
+        type = "Apartment",
+        title = "Royal Apartment",
+        address = "LosAngles LA",
+        picPath = "pic_1",
+        price = 1500,
+        bed = 2,
+        bath = 3,
+        size = 350,
+        isGarage = true,
+        score = 4.5,
+        description = "This 2 bed /3 bath home boasts an enormous, open-living plan..."
+    )
+    PropertyCard(property)
 }
