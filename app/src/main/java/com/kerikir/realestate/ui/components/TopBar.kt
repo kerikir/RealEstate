6ae9e6c54099e4ccc0b7395ef78dd5c95e02fac0
kerikir <vlasov-kerik@mail.ru>
2026-07-06 15:39:15 +0300

    added a composable function for the top panel

---
+++ app/src/main/java/com/kerikir/realestate/ui/components/TopBar.kt
@@ -0,0 +1,13 @@
+package com.kerikir.realestate.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview
+fun TopBar(
+    onBack: () -> Unit = {}
+) {
+    
+}
