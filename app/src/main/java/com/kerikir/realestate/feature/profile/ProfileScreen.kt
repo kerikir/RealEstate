f175e87c6c1738c1077610220cfc348f6bacdeaa
kerikir <vlasov-kerik@mail.ru>
2026-07-06 16:48:35 +0300

    added the user's image to the profile screen

--- app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
@@ -1,12 +1,24 @@
 package com.kerikir.realestate.feature.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -26,6 +38,25 @@ fun ProfileScreen(
         contentPadding = PaddingValues(bottom = 100.dp),
     ) {
         item { TopBar(onBack = { navController.navigateUp() }) }
+        item { Spacer(Modifier.height(16.dp)) }
+
+        item {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.profile),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .size(100.dp)
+                        .height(220.dp)
+                        .clip(RoundedCornerShape(30.dp)),
+                )
+            }
+        }
     }
 }
 
