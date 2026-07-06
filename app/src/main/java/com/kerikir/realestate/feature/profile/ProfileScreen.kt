839b257a0b7363ceae69e76a3c1505bc1b77807f
kerikir <vlasov-kerik@mail.ru>
2026-07-06 15:59:28 +0300

    added the top panel to the profile screen

--- app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
@@ -1,10 +1,16 @@
 package com.kerikir.realestate.feature.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.kerikir.realestate.R
+import com.kerikir.realestate.ui.components.TopBar
 
 
 @Composable
@@ -12,8 +18,11 @@ fun ProfileScreen(
     navController: NavController,
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.light_grey)),
+        contentPadding = PaddingValues(bottom = 100.dp),
     ) {
-
+        item { TopBar(onBack = { navController.navigateUp() }) }
     }
 }
