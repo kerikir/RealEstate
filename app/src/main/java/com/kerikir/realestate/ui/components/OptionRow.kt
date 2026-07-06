5b895d084f2e609ce6f87d191e4c0343e238804a
kerikir <vlasov-kerik@mail.ru>
2026-07-06 17:22:45 +0300

    added a description of the settings for the settings line

--- app/src/main/java/com/kerikir/realestate/ui/components/OptionRow.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/OptionRow.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kerikir.realestate.R
 
 
@@ -34,7 +37,16 @@ fun OptionRow(
             .clickable { onClick() }
             .padding(horizontal = 24.dp),
         verticalAlignment = Alignment.CenterVertically,
-    ) { }
+    ) {
+
+        Text(
+            text = title,
+            modifier = Modifier.weight(1f),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.black),
+        )
+    }
 }
 
 
