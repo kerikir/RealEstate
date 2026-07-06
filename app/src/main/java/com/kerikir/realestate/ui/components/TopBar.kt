d4b992a12c2f96b20878cfddff49cd4182438dce
kerikir <vlasov-kerik@mail.ru>
2026-07-06 15:48:59 +0300

    added a header to the top panel

--- app/src/main/java/com/kerikir/realestate/ui/components/TopBar.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/TopBar.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kerikir.realestate.R
 
 
@@ -22,7 +27,7 @@ fun TopBar(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 24.dp),
     ) {
         Image(
@@ -32,6 +37,14 @@ fun TopBar(
                 .align(Alignment.CenterStart)
                 .size(48.dp)
                 .clickable { onBack() }
+        )
+
+        Text(
+            text = "Profile",
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.black),
         )
     }
 }
