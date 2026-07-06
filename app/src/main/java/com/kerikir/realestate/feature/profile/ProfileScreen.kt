8badaf9a513d8a59ebd7db0ad3e2ac9e8273735c
kerikir <vlasov-kerik@mail.ru>
2026-07-06 15:52:17 +0300

    added a composable function for the profile screen

---
+++ app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
@@ -0,0 +1,19 @@
+package com.kerikir.realestate.feature.profile
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+
+@Composable
+fun ProfileScreen(
+    navController: NavController,
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+
+    }
+}
