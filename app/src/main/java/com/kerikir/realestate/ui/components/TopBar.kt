76f2b78dc0ebe30e676e0a727fec1fdb38075bf4
kerikir <vlasov-kerik@mail.ru>
2026-07-06 15:45:27 +0300

    added a back button to the top panel

--- app/src/main/java/com/kerikir/realestate/ui/components/TopBar.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/TopBar.kt
@@ -1,7 +1,18 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kerikir.realestate.R
 
 
 @Composable
@@ -9,5 +20,18 @@ import androidx.compose.ui.tooling.preview.Preview
 fun TopBar(
     onBack: () -> Unit = {}
 ) {
-    
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 24.dp),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.back),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .size(48.dp)
+                .clickable { onBack() }
+        )
+    }
 }
