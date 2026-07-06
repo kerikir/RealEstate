56adc9feb7a6aea9e3311d00e2786cb5a593be9c
kerikir <vlasov-kerik@mail.ru>
2026-07-06 16:52:59 +0300

    user's name has been added to the profile screen

--- app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/kerikir/realestate/feature/profile/ProfileScreen.kt
@@ -3,6 +3,7 @@ package com.kerikir.realestate.feature.profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,8 +21,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.kerikir.realestate.R
@@ -54,6 +58,21 @@ fun ProfileScreen(
                         .size(100.dp)
                         .height(220.dp)
                         .clip(RoundedCornerShape(30.dp)),
+                )
+            }
+        }
+
+        item { Spacer(Modifier.height(16.dp)) }
+        item {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Sara Anderson",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(R.color.black),
                 )
             }
         }
