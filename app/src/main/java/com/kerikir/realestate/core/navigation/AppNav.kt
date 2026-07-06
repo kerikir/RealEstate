2b29ec5426419315a7d5b227e071feae3e58f510
kerikir <vlasov-kerik@mail.ru>
2026-07-06 10:33:27 +0300

    added a composable function for preview of the bottom navigation bar

--- app/src/main/java/com/kerikir/realestate/core/navigation/AppNav.kt
+++ app/src/main/java/com/kerikir/realestate/core/navigation/AppNav.kt
@@ -1,10 +1,13 @@
 package com.kerikir.realestate.core.navigation
 
+import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
@@ -13,4 +16,18 @@ private fun BottomBar(
     modifier: Modifier = Modifier,
 ) {
     val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentDest = backStackEntry?.destination
+
+    NavigationBar(
+        modifier = modifier
+    ) { }
+}
+
+
+
+@Composable
+@Preview
+fun BottomBarPreview() {
+    val navController = rememberNavController()
+    BottomBar(navController)
 }
