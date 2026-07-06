333f9f58c29270c9bdff6f16469b29b0ca6a4ef3
kerikir <vlasov-kerik@mail.ru>
2026-07-06 17:20:01 +0300

    added a form for the settings line

--- app/src/main/java/com/kerikir/realestate/ui/components/OptionRow.kt
+++ app/src/main/java/com/kerikir/realestate/ui/components/OptionRow.kt
@@ -1,7 +1,20 @@
 package com.kerikir.realestate.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kerikir.realestate.R
 
 
 @Composable
@@ -9,7 +22,19 @@ fun OptionRow(
     title: String,
     onClick: () -> Unit = {},
 ) {
+    val white = colorResource(R.color.white)
 
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth()
+            .height(60.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .background(white)
+            .clickable { onClick() }
+            .padding(horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) { }
 }
 
 
